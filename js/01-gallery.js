af35ca9156b00279af16e03580bbce433e7bcf00
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-12 17:39:28 +0200

    ДЗ 7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const pictureGallery = document.querySelector('div.gallery');
+
+const pictureMarkup = createPictureMarkup(galleryItems);
+
+pictureGallery.insertAdjacentHTML('beforeend', pictureMarkup);
+
+pictureGallery.addEventListener('click', onPictureGalleryClick);
+
+function createPictureMarkup(pics) {
+  return pics
+    .map(({ preview, original, description }) => {
+        return `
+      <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>      
+    `;
+    })
+    .join('');
+}
+
+function onPictureGalleryClick(evt) {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName !== 'IMG') {
+    return;
+    }
+  
+  // console.log(evt.target.dataset.source);
+  
+const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" 
+    width="800" height="600"
+    >
+`)
+
+instance.show()
+}
+
 console.log(galleryItems);
+
+
